96573c0c428fab8ae112784c76216d8df278cf3e
holy-script <palashjohricool@gmail.com>
2025-01-01 14:21:48 +0530

    added camera view to home screen with react-native-vision-camera

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { StyleSheet } from 'react-native';
-import { useCameraDevice, useCameraPermission, useMicrophonePermission } from 'react-native-vision-camera';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission, useMicrophonePermission } from 'react-native-vision-camera';
 import { Redirect, router } from 'expo-router';
 
 const HomeScreen = () => {
@@ -16,7 +16,12 @@ const HomeScreen = () => {
   );
 
   if (!device) return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignContent: 'center',
+      padding: 20,
+    }}>
       <ThemedText style={{
         textAlign: 'center'
       }}>
@@ -26,11 +31,49 @@ const HomeScreen = () => {
   );
 
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText>
-        HomeScreen
-      </ThemedText>
-    </ThemedView>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{
+          flex: 2,
+          borderRadius: 10,
+          overflow: 'hidden',
+        }}>
+          <Camera
+            style={{
+              flex: 1,
+            }}
+            device={device}
+            isActive
+          />
+        </View>
+
+        <View style={{
+          flex: 1,
+        }}>
+          <View style={{
+            flex: 0.7,
+          }}>
+            <ThemedText>
+              Max FPS: {device.formats[0].maxFps}
+            </ThemedText>
+            <ThemedText>
+              Width: {device.formats[0].photoWidth}
+            </ThemedText>
+            <ThemedText>
+              Height: {device.formats[0].photoHeight}
+            </ThemedText>
+            <ThemedText>
+              Camera: {device.name}
+            </ThemedText>
+          </View>
+
+          <View style={{
+            flex: 0.7,
+          }}>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 
@@ -39,8 +82,6 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   }
 });
